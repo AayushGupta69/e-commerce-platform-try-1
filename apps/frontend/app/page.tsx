4c954df1440a613f95ffc1345cceb8c4d57d70a2
Aayush Gupta <aayushgupta69official@gmail.com>
2024-10-14 14:38:08 +0530

    Updated page.tsx

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,9 +1,14 @@
+"use client";
+
+import { Button } from "@e-commerce-platform/ui/components/ui/button";
+
 export default function Home() {
   return (
     <div className="font-bold">
       <div className="bg-red-500">Red</div>
       <div className="bg-green-500">Green</div>
       <div className="bg-blue-500">Blue</div>
+      <Button variant={"default"}>Click Me</Button>
     </div>
   );
 }
